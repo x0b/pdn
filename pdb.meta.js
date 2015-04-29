b9fa858a7f315b34f1ea79ee0b59ec9e7c80a5f3
x0b <devsignin+x0b@gmail.com>
2015-04-29 21:49:30 +0200

    First probably usable release
    - working update
    - dynamic page detection
    - improved UX: auto-scroll captcha dialog into view

---
+++ pdb.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        pyLoad Desktop Notifications
+// @namespace   https://github.com/x0b/pdn#master
+// @description This script enables Desktop Notifications for pyLoad Captchas in Firefox. Note: This app is in pre-alpha state. Do not use it unless you know exactly what you are doing.
+// @match       http://localhost:8000/*
+// @match       http://localhost:8001/*
+// @version     0.1.1a
+// @downloadURL https://raw.githubusercontent.com/x0b/pdn/master/pdn.user.js
+// @updateURL   https://raw.githubusercontent.com/x0b/pdn/master/pdn.meta.js
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
